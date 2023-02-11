914feb9d3c88da3dd8983cc2393e13db2a9d30cc
delurintu <georgiana_delurintu@yahoo.com>
2023-02-11 18:19:57 +0200

    wip taskA and taskB

---
+++ ASSUMPTIONS.md
@@ -0,0 +1,12 @@
+Task A.
+
+- I wanted to create a RegEx to validate the form input
+- The commonExpressionsRegExp only validates the common expression, i created one for the advanced expression (sin, cos and tan) but for now doesn t work properly
+
+Task B.
+
+- i used spilt() method with a regex to separate the expression and put the elements in an array to use the slice() to extract the them.
+- i verified the each element and make the calculation where it finds correct operators
+- for now this works for common operators
+
+// TODO, extend funtionality for advandes operators (sin,cos and tan)
--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-app';
+  expression: FormControl;
+
+  commonExpressionsRegExp='^[-+]?[0-9]*\.?[0-9]+([-+*/]?([0-9]*\.?[0-9]+))*$'
+  advancedExpressionsRegExp='^((sin|cos|tan+([0-9]+)))$'
+
+   constructor(){
+    this.expression = new FormControl('',[Validators.required, Validators.pattern(this.commonExpressionsRegExp)]);
+
+   }
+
+   validateExp() {
+    console.log("Valid? :"+this.expression.valid)
+    console.log("Evaluate expresion", this.solveExpression(this.expression.value))
+   }
+
+   splitExpressionValue(value:any){
+    value = value.slice();
+
+    while(value.length-1){
+      if(value[1] === '*') value[0] = value[0] * value[2]
+      if(value[1] === '-') value[0] = value[0] - value[2]
+      if(value[1] === '+') value[0] = +value[0] + (+value[2])
+      if(value[1] === '/') value[0] = value[0] / value[2]
+      value.splice(1,1);
+      value.splice(1,1);
+    }
+    return value[0];
+  }
+  
+  solveExpression(eq:any) {
+    let res = eq.split(/(\+|-)/g).map((x: string) => x.trim().split(/(\*|\/)/g).map(a => a.trim()));
+    res = res.map((x: any) => this.splitExpressionValue(x)); 
+     
+    return this.splitExpressionValue(res)
+  
+  }
 }
